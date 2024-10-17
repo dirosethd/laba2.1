d822dedae31ff1efb897165872db7bce2b9fb107
dirose <dirose@DESKTOP-PQA70RD>
2024-10-17 21:55:15 +0200

    Добавьте файлы проекта.

---
+++ laba2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿try
+{  
+  Console.WriteLine("Введите число:");
+    int a = int.Parse(Console.ReadLine());
+    if ((a % 10 == 0) && (a % 3 != 0)) Console.WriteLine("Правильно");
+    else Console.WriteLine("Неправильно");
+}
+catch
+{
+    Console.WriteLine("Введите правильно");
+}
+
